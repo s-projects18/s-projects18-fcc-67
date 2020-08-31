6077288c0db161b212c21faaa86ca5f5d66899d9
sprojects18 <info@sgenschow.de>
2020-08-31 08:45:50 +0000

    Version1: all tests running

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ cracked_password1 = password_cracker.crack_sha1_hash(
     "fbbe7e952d1050bfb09dfdb71d4c2ff2b3d845d2")
 print(cracked_password1)
 
+# superman
 cracked_password2 = password_cracker.crack_sha1_hash(
-    "dcc466796201f7232b22a03781110a8871fd038c", use_salts=True)
+    "53d8b3dc9d39f0184144674e310185e41a87ffd5", use_salts=True)
 print(cracked_password2)
 
 # Run unit tests automatically
--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,55 @@
 import hashlib
 
-def crack_sha1_hash(hash):
-    
+def get_list(path):
+  """import file and return ist as a list"""
+  try:
+    file_handle = open(path, 'r')
+    l = file_handle.read().splitlines(0)
+    file_handle.close()
+    return l
+  except:
+    print("Error: Can't open file: ", path)  
+    exit()
+
+def check_hash(pw, hash):
+  """create sha1-hash of pw and compare it with hash"""
+  h = hashlib.new("sha1") # hash-generator
+  h.update(pw.encode('ascii'))
+  hashed = h.hexdigest()
+  if hash==hashed:
+    return True  
+  return False
+
+def crack_sha1_hash(hash, use_salts=False):
+  """hash: SHA-1 hash of a password
+
+  return the password if it is one of the top 10,000 passwords used"""  
+  pathPw    = 'top-10000-passwords.txt'
+  pathSalts = 'known-salts.txt'
+
+  # read complete salt-list
+  if use_salts==True:
+    salts = get_list(pathSalts)
+
+  # read complete password-list
+  # (using a handle and iterating over it
+  # leads to memory-warnings in repl.it)
+  passwords = get_list(pathPw)
+
+  # check paswords
+  for pw in passwords:
+    if use_salts:
+      pw_var = []
+      # NOT: salt + pw + salt
+      for salt in salts:
+        pw_var.append(pw + salt)
+        pw_var.append(salt + pw)
+      for pws in pw_var:  
+        if check_hash(pws, hash)==True:
+          return pw
+    else:
+      if check_hash(pw, hash)==True:
+        return pw
+
+  return "PASSWORD NOT IN DATABASE"
+
